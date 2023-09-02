48e1c6360442ba8b785f1a7559f46feba3047a0d
junhyeok <toughhyeok96@gmail.com>
2023-09-02 21:52:39 +0900

    [refactoring] create interface and using spring DI

--- src/main/java/hotamulspring/helloboot/HelloController.java
+++ src/main/java/hotamulspring/helloboot/HelloController.java
@@ -3,8 +3,13 @@ package hotamulspring.helloboot;
 import java.util.Objects;
 
 public class HelloController {
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
     public String hello(String name) {
-        SimpleHelloService helloService = new SimpleHelloService();
         return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
