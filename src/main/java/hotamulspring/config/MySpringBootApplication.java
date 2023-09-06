b2db9f67f5421e9c7f62fd45260df5377824e927
junhyeok <toughhyeok96@gmail.com>
2023-09-06 23:43:43 +0900

    [refactoring] deferred import selector

--- src/main/java/hotamulspring/config/MySpringBootApplication.java
+++ src/main/java/hotamulspring/config/MySpringBootApplication.java
@@ -1,6 +1,5 @@
 package hotamulspring.config;
 
-import hotamulspring.config.EnableMyAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
