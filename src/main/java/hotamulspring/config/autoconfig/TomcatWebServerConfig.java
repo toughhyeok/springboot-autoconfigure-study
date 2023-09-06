7349e0bfbf249cb55b58ff438861022196404815
junhyeok <toughhyeok96@gmail.com>
2023-09-07 00:00:13 +0900

    [refactoring] configuration proxyBeanMethods false

--- src/main/java/hotamulspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/hotamulspring/config/autoconfig/TomcatWebServerConfig.java
@@ -1,9 +1,11 @@
 package hotamulspring.config.autoconfig;
 
+import hotamulspring.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
+@MyAutoConfiguration
 public class TomcatWebServerConfig {
     @Bean
     public ServletWebServerFactory servletWebServerFactory() {
