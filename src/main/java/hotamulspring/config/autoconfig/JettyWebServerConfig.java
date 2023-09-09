e9f043bcc236322f0972c2e68abaf185591ddad5
junhyeok <toughhyeok96@gmail.com>
2023-09-09 23:34:00 +0900

    [refactoring] select webserver using spring class utils

--- src/main/java/hotamulspring/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/hotamulspring/config/autoconfig/JettyWebServerConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.util.ClassUtils;
 
 @MyAutoConfiguration
 @Conditional(JettyWebServerConfig.JettyCondition.class)
@@ -20,7 +21,7 @@ public class JettyWebServerConfig {
     static class JettyCondition implements Condition {
         @Override
         public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return true;
+            return ClassUtils.isPresent("org.eclipse.jetty.server.Server", context.getClassLoader());
         }
     }
 }
--- src/main/java/hotamulspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/hotamulspring/config/autoconfig/TomcatWebServerConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.util.ClassUtils;
 
 @MyAutoConfiguration
 @Conditional(TomcatWebServerConfig.TomcatCondition.class)
@@ -20,7 +21,7 @@ public class TomcatWebServerConfig {
     static class TomcatCondition implements Condition {
         @Override
         public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return false;
+            return ClassUtils.isPresent("org.apache.catalina.startup.Tomcat", context.getClassLoader());
         }
     }
 }
