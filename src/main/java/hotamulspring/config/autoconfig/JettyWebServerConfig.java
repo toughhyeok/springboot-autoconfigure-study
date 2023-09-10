6e1497097d1b491d1b5e542bd20dcf988c865871
junhyeok <toughhyeok96@gmail.com>
2023-09-11 00:09:05 +0900

    [refactoring] using value annotation get property

--- src/main/java/hotamulspring/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/hotamulspring/config/autoconfig/JettyWebServerConfig.java
@@ -2,6 +2,7 @@ package hotamulspring.config.autoconfig;
 
 import hotamulspring.config.ConditionalMyOnClass;
 import hotamulspring.config.MyAutoConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -11,11 +12,14 @@ import org.springframework.core.env.Environment;
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 public class JettyWebServerConfig {
+    @Value("${contextPath}")
+    String contextPath;
+
     @Bean("jettyWebServerFactory")
     @ConditionalOnMissingBean
-    public ServletWebServerFactory servletWebServerFactory(Environment env) {
+    public ServletWebServerFactory servletWebServerFactory() {
         JettyServletWebServerFactory factory = new JettyServletWebServerFactory();
-        factory.setContextPath(env.getProperty("contextPath"));
+        factory.setContextPath(this.contextPath);
         return factory;
     }
 
---
+++ src/main/java/hotamulspring/config/autoconfig/PropertyPlaceholderConfig.java
@@ -0,0 +1,13 @@
+package hotamulspring.config.autoconfig;
+
+import hotamulspring.config.MyAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@MyAutoConfiguration
+public class PropertyPlaceholderConfig {
+    @Bean
+    PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
