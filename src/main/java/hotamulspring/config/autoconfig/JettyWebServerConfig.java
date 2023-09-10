94e5f8f09193060c303aa3394f42ee33ce3fc4f2
junhyeok <toughhyeok96@gmail.com>
2023-09-11 00:00:46 +0900

    [feature] set context path using Environment Bean

--- src/main/java/hotamulspring/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/hotamulspring/config/autoconfig/JettyWebServerConfig.java
@@ -6,14 +6,17 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 public class JettyWebServerConfig {
     @Bean("jettyWebServerFactory")
     @ConditionalOnMissingBean
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new JettyServletWebServerFactory();
+    public ServletWebServerFactory servletWebServerFactory(Environment env) {
+        JettyServletWebServerFactory factory = new JettyServletWebServerFactory();
+        factory.setContextPath(env.getProperty("contextPath"));
+        return factory;
     }
 
 }
